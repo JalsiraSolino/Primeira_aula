fe904c114159f5d42f8a5639b4b8e7865625c31c
Jacinta Silvino Solino <jacinta.solino@tis.ao>
2025-12-07 19:17:27 +0100

    Primeiro comit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'segunda_pratica';
-  nome = 'jacinta Silvino Solino'
-  idade = 50
-  sexo = 'F'
+  nome = 'Jacinta Silvino Solino'
+  endereco = 'Vil de viana/ Luand Sul'
+  email = 'jlsirsolino1@gmail.com'
+  telefone = '928711535'
+  
+display: boolean = true
+
 }
